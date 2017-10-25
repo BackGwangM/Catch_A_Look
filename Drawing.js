d8cec2fa9eedc9ff945707c1ba48aaff51db5241
back_gwang_min <back5373122@gmail.com>
2017-10-25 23:13:55 +0900

    원 삭제
    용량이 매우 줄어들거야!

---
+++ Drawing.js
@@ -0,0 +1,3 @@
+var screen_h = screen.height;
+
+document.write("<div id='canvas_border' style='width:"+screen_h*1.2+"px'><canvas id='c' height='"+screen_h*0.6+"' width='"+screen_h*1.2+"'></canvas></div>");
