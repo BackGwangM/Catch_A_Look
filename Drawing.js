bd80b47a8b5eb93af063ed598c0be9ddca51c7b8
back_gwang_min <back5373122@gmail.com>
2017-10-26 16:17:04 +0900

    익스플로어 방지 적용

--- Drawing.js
+++ Drawing.js
@@ -1,7 +1,8 @@
 var screen_h = screen.height;
 
 if(navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1){
-    alert("익스플로어 11에서는 정상적인 작동이 되지 않습니다. 엣지 환경에서 실행해주세요");   
+    alert("익스플로어 11에서는 정상적인 작동이 되지 않습니다. 엣지 환경에서 실행해주세요");
+    history.back();
 }
 
 
