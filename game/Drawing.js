f24dd42188dcda2672e51427658e421c7eb7f52c
back_gwang_min <back5373122@gmail.com>
2017-11-30 13:44:32 +0900

    add waiting_room

--- game/Drawing.js
+++ game/Drawing.js
@@ -4,5 +4,3 @@ if(navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -
     alert("익스플로어 11에서는 정상적인 작동이 되지 않습니다. 다른 브라우저 환경에서 실행해주세요");
     history.back();
 }
-
-document.write("<div id='canvas_border' style='width:"+screen_h*1.30+"px'><canvas id='c' height='"+screen_h*0.65+"' width='"+screen_h*1.30+"'></canvas></div>");
